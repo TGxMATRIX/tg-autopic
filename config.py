244306c3c1af851c47778bf1c3ffdc0c9e6eeb9d
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-06-21 13:31:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from os import environ
 class Config:
     USER_TOKEN = os.environ.get("USER_TOKEN", None)
 
-    API_ID = int(environ.get("APP_ID"))
+    API_ID = int(environ['API_ID'])
 
     API_HASH = os.environ.get("API_HASH", None)
     
-    SAVE_TO_CHNL = int(environ.get("SAVE_TO_CHNL"))
+    SAVE_TO_CHNL = int(environ["SAVE_TO_CHNL"])
